ea417ea4c46cdc1b62ce251220745fc2adc0d8e6
Everton <everton.jiujitsu@gmail.com>
2018-01-24 00:44:21 +0000

    Adding BotFramework emulator.

--- code/pratic-tiro-chatbot-nodejs/app.js
+++ code/pratic-tiro-chatbot-nodejs/app.js
@@ -1,6 +1,26 @@
+var restify = require('restify');
 var builder = require('botbuilder');
 
-var connector = new builder.ConsoleConnector().listen();
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Crie um chat conector para se comunicar com o Bot Framework Service
+var connector_id = {
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+};
+
+connector_id = {};
+var connector = new builder.ChatConnector(connector_id);
+
+// Endpoint que irá monitorar as mensagens do usuário
+server.post('/api/messages', connector.listen());
+
+// Recebe as mensagens do usuário e responde repetindo cada mensagem (prefixado com 'Você disse:')
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("Você disse: %s", session.message.text);
 });
+
